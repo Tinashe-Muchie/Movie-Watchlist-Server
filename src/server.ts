12080f55c9ac630d0cbc56c89e368796fc71913f
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-09-02 10:47:17 +0200

    .

--- src/server.ts
+++ src/server.ts
@@ -236,6 +236,7 @@ const resolvers = {
         /*returns an array of either TvShows or movies depending on the client's input*/
         search: async ( _: unknown, {name}:{name: string}, {dataSources}: {dataSources: any}) => {
             const data: MovieIF[] | TvShowIF[] = await dataSources.movieAPI.Search(name);
+            if(Array.isArray(data)){
                 return data.map((item) => {
                     if(item.media_type === 'movie') return {
                         __typename: 'Movies',
@@ -247,7 +248,7 @@ const resolvers = {
                         ...item
                     }
                 });
-            
+            }   
         }
     },
 
