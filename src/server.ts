d768f6cbfc1a5168cb8d5f58c640bb8a8fe6bef7
Tinashe Muchineripi <73638846+Tinashe-Muchie@users.noreply.github.com>
2021-09-03 16:42:44 +0200

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -255,8 +255,8 @@ const resolvers = {
         },
         /*returns an array of either TvShows or movies depending on the client's input*/
         search: async ( _: unknown, {name}:{name: string}, {dataSources}: {dataSources: any}) => {
-            const data: MovieIF[] | TvShowIF[] = await dataSources.movieAPI.Search(name);
-            return data.map((item: MovieIF | TvShowIF) => {
+            const data = await dataSources.movieAPI.Search(name);
+            return data.map((item) => {
                 if(item.media_type === 'movie') return {
                     __typename: 'Movies',
                     ...item
